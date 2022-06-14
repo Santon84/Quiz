40950c48c2a529fee0f26d691aebc9fe1b88484a
Santon84 <102661640+Santon84@users.noreply.github.com>
2022-06-14 21:58:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,528 @@
+
+const QUIZ_DATA1 = [
+{
+    question: "Дерево с желудями – это...",
+    answers: [
+            {
+                id: 1,
+                value: "Бук",
+                correct: false,
+            } ,
+            {
+                id: 2,
+                value: "Клен",
+                correct: false,
+            },
+            {
+                id: 3,
+                value: "Дуб",
+                correct: true,
+            },
+            {
+                id: 4,
+                value: "Ель",
+                correct: false,
+            }
+    ]    
+    
+}, 
+{
+    question: "Животное с домиком на спине – это …?",
+    answers: [
+            {
+                id: 1,
+                value: "Улитка",
+                correct: true,
+            } ,
+            {
+                id: 2,
+                value: "Медуза",
+                correct: false,
+            },
+            {
+                id: 3,
+                value: "Жук",
+                correct: false,
+            },
+            {
+                id: 4,
+                value: "Головастик",
+                correct: false,
+            }
+    ]    
+    
+}, 
+{
+    question: "Какой последний месяц лета?",
+    answers: [
+            {
+                id: 1,
+                value: "Август",
+                correct: true,
+            } ,
+            {
+                id: 2,
+                value: "Март",
+                correct: false,
+            },
+            {
+                id: 3,
+                value: "Январь",
+                correct: false,
+            },
+            {
+                id: 4,
+                value: "Май",
+                correct: false,
+            }
+    ]    
+    
+}
+, 
+{
+    question: "Как называются каникулы у родителей?",
+    answers: [
+            {
+                id: 1,
+                value: "Выходной",
+                correct: false,
+            } ,
+            {
+                id: 2,
+                value: "Каникулы",
+                correct: false,
+            },
+            {
+                id: 3,
+                value: "Отпуск",
+                correct: true,
+            },
+            {
+                id: 4,
+                value: "Вечеринка",
+                correct: false,
+            }
+    ]    
+    
+}
+, 
+{
+    question: "В чём дети носят книги в школу?",
+    answers: [
+            {
+                id: 1,
+                value: "Мешок",
+                correct: false,
+            } ,
+            {
+                id: 2,
+                value: "В руках",
+                correct: false,
+            },
+            {
+                id: 3,
+                value: "Портфель",
+                correct: true,
+            },
+            {
+                id: 4,
+                value: "Чемодан",
+                correct: false,
+            }
+    ]    
+    
+}
+, 
+{
+    question: "Как назывался корабль у котов в книге",
+    answers: [
+            {
+                id: 1,
+                value: "КИСОБОЙ",
+                correct: false,
+            } ,
+            {
+                id: 2,
+                value: "КОТОБОЙ",
+                correct: true,
+            },
+            {
+                id: 3,
+                value: "КИТОБОЙ",
+                correct: false,
+            },
+            {
+                id: 4,
+                value: "КОРОБОЙ",
+                correct: false,
+            }
+    ]    
+    
+}
+, 
+{
+    question: "Как зовут зайца в смешариках",
+    answers: [
+            {
+                id: 1,
+                value: "Кроляш",
+                correct: false,
+            } ,
+            {
+                id: 2,
+                value: "Шустер",
+                correct: false,
+            },
+            {
+                id: 3,
+                value: "Крош",
+                correct: true,
+            },
+            {
+                id: 4,
+                value: "Попрыгун",
+                correct: false,
+            }
+    ]    
+    
+}
+, 
+{
+    question: "Какая машина может копать глубокие ямы",
+    answers: [
+            {
+                id: 1,
+                value: "Кран",
+                correct: false,
+            } ,
+            {
+                id: 2,
+                value: "Бульдозер",
+                correct: false,
+            },
+            {
+                id: 3,
+                value: "Танк",
+                correct: false,
+            },
+            {
+                id: 4,
+                value: "Экскаватор",
+                correct: true,
+            }
+    ]    
+    
+}
+
+]
+
+const QUIZ_DATA = [
+    {
+        question: "Человек кушает ртом, а птицы…",
+        answers: [
+                {
+                    id: 1,
+                    value: "Крыльями",
+                    correct: false,
+                } ,
+                {
+                    id: 2,
+                    value: "Носом",
+                    correct: false,
+                },
+                {
+                    id: 3,
+                    value: "Клювом",
+                    correct: true,
+                },
+                {
+                    id: 4,
+                    value: "Ногами",
+                    correct: false,
+                }
+        ]    
+        
+    }, 
+    {
+        question: "Сколько месяцев в году?",
+        answers: [
+                {
+                    id: 1,
+                    value: "12",
+                    correct: true,
+                } ,
+                {
+                    id: 2,
+                    value: "14",
+                    correct: false,
+                },
+                {
+                    id: 3,
+                    value: "10",
+                    correct: false,
+                },
+                {
+                    id: 4,
+                    value: "24",
+                    correct: false,
+                }
+        ]    
+        
+    }, 
+    {
+        question: "В каком числе 6 нулей?",
+        answers: [
+                {
+                    id: 1,
+                    value: "Сто тысяч",
+                    correct: false,
+                } ,
+                {
+                    id: 2,
+                    value: "Миллиард",
+                    correct: false,
+                },
+                {
+                    id: 3,
+                    value: "Тысяча",
+                    correct: false,
+                },
+                {
+                    id: 4,
+                    value: "Миллион",
+                    correct: true,
+                }
+        ]    
+        
+    }
+    , 
+    {
+        question: "Как называется прогулка в музее с гидом?",
+        answers: [
+                {
+                    id: 1,
+                    value: "Путеводитель",
+                    correct: false,
+                } ,
+                {
+                    id: 2,
+                    value: "Обучающая прогулка",
+                    correct: false,
+                },
+                {
+                    id: 3,
+                    value: "Экскурсия",
+                    correct: true,
+                },
+                {
+                    id: 4,
+                    value: "Экзекуция",
+                    correct: false,
+                }
+        ]    
+        
+    }
+    , 
+    {
+        question: "Владельцу какой шапки подчинялись летучие обезьяны из сказки «Волшебник Изумрудного города»?",
+        answers: [
+                {
+                    id: 1,
+                    value: "Бронзовой",
+                    correct: false,
+                } ,
+                {
+                    id: 2,
+                    value: "Серебряной",
+                    correct: false,
+                },
+                {
+                    id: 3,
+                    value: "Красной",
+                    correct: false,
+                },
+                {
+                    id: 4,
+                    value: "Золотой",
+                    correct: true,
+                }
+        ]    
+        
+    }
+    , 
+    {
+        question: "Что надо было сделать, чтобы серебряные башмачки Гингемы перенесли туда, куда пожелаешь?",
+        answers: [
+                {
+                    id: 1,
+                    value: "Трижды подпрыгнуть",
+                    correct: false,
+                } ,
+                {
+                    id: 2,
+                    value: "Стукнуть каблуком о каблук ",
+                    correct: true,
+                },
+                {
+                    id: 3,
+                    value: "Повернуться на каблуке вокруг себя",
+                    correct: false,
+                },
+                {
+                    id: 4,
+                    value: "Хлопнуть в ладоши",
+                    correct: false,
+                }
+        ]    
+        
+    }
+    , 
+    {
+        question: "Как ребята называли полицейского из книги о 5ти юных сыщиках ",
+        answers: [
+                {
+                    id: 1,
+                    value: "Полицай",
+                    correct: false,
+                } ,
+                {
+                    id: 2,
+                    value: "Дядя Степа",
+                    correct: false,
+                },
+                {
+                    id: 3,
+                    value: "Мистер Гун",
+                    correct: false,
+                },
+                {
+                    id: 4,
+                    value: "А ну ка разойдить",
+                    correct: true,
+                }
+        ]    
+        
+    }
+    , 
+    {
+        question: "Какой герой летал на пушечном ядре",
+        answers: [
+                {
+                    id: 1,
+                    value: "Мэри Поппинс",
+                    correct: false,
+                } ,
+                {
+                    id: 2,
+                    value: "Маугли",
+                    correct: false,
+                },
+                {
+                    id: 3,
+                    value: "Леди Баг",
+                    correct: false,
+                },
+                {
+                    id: 4,
+                    value: "Барон Мюнхаузен",
+                    correct: true,
+                }
+        ]    
+        
+    }
+    
+    ]
+
+
+let quizHeader = document.querySelector('.quiz__header');
+let answers = document.querySelector('.quiz__answers');
+let progress = document.querySelector('.quiz-progress');
+let answerButton = document.querySelector('.quiz-button');
+let pointSum = 0;
+
+let currentAnswer = false;
+let index = 0;
+
+const renderQuestion = () => {
+
+    quizHeader.innerHTML = QUIZ_DATA[index].question; 
+    
+    }
+
+
+// const renderBackground = () => {
+//         let conteiner = document.querySelector('.conteiner')
+//         console.log(conteiner)
+//         conteiner.style.backgroundImage = `url(${images[index]})`;
+
+//         }
+    
+const renderAnswers = () => {
+    answers.innerHTML = '';
+    answerButton.disabled = true;
+    QUIZ_DATA[index].answers.forEach(answer => {
+    answers.innerHTML +=`
+    <div class="quiz__answer">
+        <input type="radio" class="quiz-answer__radio" id="${answer.id}"
+        name="question">
+        <label for="${answer.id}">${answer.value}</label>
+    </div>
+    `
+    })
+    
+    let radios = document.querySelectorAll('.quiz-answer__radio');
+    radios.forEach(radio => {
+    radio.addEventListener('change', (evt) =>{
+    currentAnswer = false
+    
+    if (QUIZ_DATA[index].answers[radio.id-1].correct) {currentAnswer = true} 
+    else {currentAnswer = false};
+    if (answerButton.disabled) {answerButton.disabled = false;}
+    })
+
+} )
+
+  }    
+const renderProgress = () => {
+
+    let progressLabel = index === QUIZ_DATA.length ? 'Вопросы кончились!' : `Вопросов ${index} / ${QUIZ_DATA.length}`;
+    
+
+    progress.innerHTML = `<label for="progress">${progressLabel} </label>
+                        <progress id="progress" max="${QUIZ_DATA.length}" value="${index}"></progress>
+     `;
+}
+
+const renderResults = () => {
+    answers.innerHTML = `<p>Молодец! Твой результат ${pointSum} из ${QUIZ_DATA.length}!</p>`;
+}
+
+answerButton.addEventListener("click", function () {
+
+if (currentAnswer) {pointSum++};
+
+
+    if (index === QUIZ_DATA.length-1) {
+        index++;
+        renderResults();
+        renderProgress();
+        
+        quizHeader.textContent = "Конец";
+        answerButton.textContent = "Заново"
+        answerButton.addEventListener("click", function () {
+            window.location.reload();
+        })
+    } else {
+    index++;
+    renderQuestion();
+    renderAnswers();
+    renderProgress();
+    }
+})
+
+
+renderQuestion();
+renderAnswers();
+renderProgress();
+
+renderBackground();
